085ee15d92d48b15c98af3f618ecd740b95ecd81
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-04-11 17:15:44 -0600

    main titpe updates

--- apps/angular-app/src/app/components/main-title/main-title.component.ts
+++ apps/angular-app/src/app/components/main-title/main-title.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
+import { FilterButtonGroupComponent } from '../filter-button-group/filter-button-group.component';
 
 
 @Component({
   selector: 'mfee-project-main-title',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, FilterButtonGroupComponent],
   templateUrl: './main-title.component.html',
   styleUrl: './main-title.component.scss'
 })

a050722b9f6bceadc0e3f779205ec3ca9ea2adbc
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-04-11 13:42:10 -0600

    filter button group component creation

---
+++ apps/angular-app/src/app/components/filter-button-group/filter-button-group.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mfee-project-filter-button-group',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './filter-button-group.component.html',
+  styleUrl: './filter-button-group.component.scss'
+})
+export class FilterButtonGroupComponent {}

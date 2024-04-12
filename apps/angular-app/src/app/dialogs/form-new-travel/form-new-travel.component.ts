513aa7221ea893df27ed8bc4cb98245070c4787a
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-04-12 08:47:25 -0600

    reactive form changes

--- apps/angular-app/src/app/dialogs/form-new-travel/form-new-travel.component.ts
+++ apps/angular-app/src/app/dialogs/form-new-travel/form-new-travel.component.ts
@@ -6,22 +6,38 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { HomeComponent } from '../../views/home/home.component';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mfee-project-form-new-travel',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatSelectModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatSelectModule, ReactiveFormsModule],
   templateUrl: './form-new-travel.component.html',
   styleUrl: './form-new-travel.component.scss'
 })
 export class FormNewTravelComponent {
-  constructor(public dialogRef: MatDialogRef<HomeComponent>, @Inject(MAT_DIALOG_DATA) public data:unknown) {}
+  constructor(public dialogRef: MatDialogRef<HomeComponent>, @Inject(MAT_DIALOG_DATA) public data: unknown) {}
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  profileForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    description: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    category: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    url: new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
+
+  onSubmit() {
+    if (this.profileForm.valid) {
+      console.log(this.profileForm.value);
+    } else {
+      console.log(' Algun campo no es valido');
+    }
   }
 }

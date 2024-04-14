2692f3d72e29cf6bb9b644cb79c3371dd00bbaf0
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-04-14 02:28:22 -0600

    world-card effects, styles and databinding updates

--- apps/angular-app/src/app/components/world-card/world-card.component.ts
+++ apps/angular-app/src/app/components/world-card/world-card.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { HttpComunicationService } from '../../services/http-comunication.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'mfee-project-world-card',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatCardModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule, HttpClientModule],
+  providers: [HttpComunicationService],
   templateUrl: './world-card.component.html',
   styleUrl: './world-card.component.scss'
 })
-export class WorldCardComponent {}
+export class WorldCardComponent implements OnInit {
+  title: string;
+  comments: string;
+  description: string;
+  imageUrl: string;
+
+  constructor(public _http: HttpComunicationService) {}
+  ngOnInit() {
+    this.title = 'mi primer titulo';
+    this.comments = 'mi primer comments';
+    this.description = 'mi primer description';
+    this.imageUrl = 'https://material.angular.io/assets/img/examples/shiba2.jpg';
+  }
+}

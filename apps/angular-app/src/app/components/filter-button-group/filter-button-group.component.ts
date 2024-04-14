bed24e12210df5c00e2ab128295fc1944f84f998
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-04-14 02:30:48 -0600

    map, pipe and filter for each section

--- apps/angular-app/src/app/components/filter-button-group/filter-button-group.component.ts
+++ apps/angular-app/src/app/components/filter-button-group/filter-button-group.component.ts
@@ -1,11 +1,110 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpComunicationService } from '../../services/http-comunication.service';
+import { map } from 'rxjs';
+
 
 @Component({
   selector: 'mfee-project-filter-button-group',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
+  providers: [HttpComunicationService],
   templateUrl: './filter-button-group.component.html',
   styleUrl: './filter-button-group.component.scss'
 })
-export class FilterButtonGroupComponent {}
+export class FilterButtonGroupComponent {
+  titles: any[] = [];
+
+  constructor(public _http: HttpComunicationService) {}
+
+  // getAllData() {
+  //   this._http.getAllData().subscribe((x) => {
+  //     console.log(x);
+  //   });
+  // }
+
+  getAllData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) =>
+          response.map((item) => ({
+            id: item.id,
+            title: item.title,
+            description: item.description,
+            category: item.category,
+            url: item.url
+          }))
+        )
+      )
+      .subscribe((x) => {
+        console.log(x);
+      });
+  }
+
+  getAllTravelData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Travel')
+      )
+      )
+      .subscribe((x) => {
+        console.log(x);
+      });
+  }
+
+  getAllLifestyleData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Lifestyle')
+      )
+      )
+      .subscribe((x) => {
+        console.log(x);
+      });
+  }
+
+  getAllBusinessData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Business')
+      )
+      )
+      .subscribe((x) => {
+        console.log(x);
+      });
+  }
+
+  getAllFoodData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Food')
+      )
+      )
+      .subscribe((x) => {
+        console.log(x);
+      });
+  }
+
+  getAllWorkData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Work')
+      )
+      )
+      .subscribe((x) => {
+        console.log(x);
+      });
+  }
+}

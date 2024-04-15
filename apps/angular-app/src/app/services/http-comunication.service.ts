45731583952cea03dd8362502c1eac9e6263cf84
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-04-15 10:34:14 -0600

    interface name correction

--- apps/angular-app/src/app/services/http-comunication.service.ts
+++ apps/angular-app/src/app/services/http-comunication.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { travel } from '../interfaces/enums';
+import { travel } from '../interfaces/interfaces';
 
 
 @Injectable({

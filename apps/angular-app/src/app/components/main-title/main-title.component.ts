32d545cc5f361b26ffa3ecfc093a8b3565f736a1
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-04-16 09:25:25 -0600

    filter buttom group deleted

--- apps/angular-app/src/app/components/main-title/main-title.component.ts
+++ apps/angular-app/src/app/components/main-title/main-title.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
-import { FilterButtonGroupComponent } from '../filter-button-group/filter-button-group.component';
+
 import { FormNewTravelComponent } from '../../dialogs/form-new-travel/form-new-travel.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'mfee-project-main-title',
   standalone: true,
-  imports: [CommonModule, MatIconModule, FilterButtonGroupComponent],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './main-title.component.html',
   styleUrl: './main-title.component.scss'
 })

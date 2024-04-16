53c789aa06b2ba70d60e0157d9ccde566092427b
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-04-16 09:27:43 -0600

    buttons group to filter into world card to make the refresh auto

--- apps/angular-app/src/app/components/world-card/world-card.component.ts
+++ apps/angular-app/src/app/components/world-card/world-card.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { HttpComunicationService } from '../../services/http-comunication.service';
 import { HttpClientModule } from '@angular/common/http';
+import { map } from 'rxjs';
+import { travel } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'mfee-project-world-card',
@@ -20,6 +22,8 @@ export class WorldCardComponent implements OnInit {
   url: string;
   cards: any;
   @Input() cardData;
+  @Output() worldInfoEvent = new EventEmitter()
+  travelInfo: travel
 
   constructor(public _http: HttpComunicationService) {}
   ngOnInit() {
@@ -30,6 +34,104 @@ export class WorldCardComponent implements OnInit {
   }
 
   refresh() {
-    this.cards = this.cardData;
+    this.cards = this.travelInfo;
+  }
+
+  getAllData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) =>
+          response.map((item) => ({
+            id: item.id,
+            title: item.title,
+            description: item.description,
+            category: item.category,
+            url: item.url
+          }))
+        )
+      )
+      .subscribe((x) => {
+        this.travelInfo = x
+        this.sendWorldInfo();
+      });
+  }
+
+  getAllTravelData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Travel')
+      )
+      )
+      .subscribe((x) => {
+        this.travelInfo = x
+        this.sendWorldInfo();
+      });
+  }
+
+  getAllLifestyleData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Lifestyle')
+      )
+      )
+      .subscribe((x) => {
+        this.travelInfo = x
+        this.sendWorldInfo();
+      });
+  }
+
+  getAllBusinessData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Business')
+      )
+      )
+      .subscribe((x) => {
+        this.travelInfo = x
+        this.sendWorldInfo();
+      });
+  }
+
+  getAllFoodData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Food')
+      )
+      )
+      .subscribe((x) => {
+        this.travelInfo = x
+        this.sendWorldInfo();
+      });
+  }
+
+  getAllWorkData() {
+    return this._http
+      .getAllData()
+      .pipe(
+        map((response: any) => 
+          response.filter((item: any) => item.category === 'Work')
+      )
+      )
+      .subscribe((x) => {
+        this.travelInfo = x
+        this.sendWorldInfo();
+      });
+  }
+
+  sendWorldInfo() {
+    this.worldInfoEvent.emit({ mensaje: this.travelInfo })
+    console.log(this.travelInfo)
+    setTimeout(() => {
+      this.refresh()
+    }, 100);
   }
 }

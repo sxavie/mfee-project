900d2b84578d7be00e7773f15b1ee00b804a29cd
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-05-02 13:28:10 -0600

    navigation section

--- apps/angular-app/src/app/views/home/home.component.ts
+++ apps/angular-app/src/app/views/home/home.component.ts
@@ -3,20 +3,40 @@ import { CommonModule } from '@angular/common';
 import { MainTitleComponent } from '../../components/main-title/main-title.component';
 
 import { WorldCardComponent } from '../../components/world-card/world-card.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mfee-project-home',
   standalone: true,
-  imports: [CommonModule, MainTitleComponent, WorldCardComponent],
+  imports: [CommonModule, MainTitleComponent, WorldCardComponent, MatMenuModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   cardData: any;
   cardDataToChild: any;
+  constructor(private router: Router) {}
 
   reciveWorldInfoEvent(event: any) {
     this.cardData = event.mensaje;
-    this.cardDataToChild = event.mensaje
+    this.cardDataToChild = event.mensaje;
+  }
+
+  navTo(route) {
+    this.router.navigate(['/login']);
+    switch (route) {
+      case 'login':
+        console.log('to login')
+        this.router.navigate(['/login']);
+        break;
+      case 'categories':
+        console.log('to categories')
+        this.router.navigate(['/categories']);
+        break;
+      default:
+        this.router.navigate(['/login']); 
+    }
   }
 }

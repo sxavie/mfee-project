8f641782a9d3fcba6335e7334c7d89540a41996c
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-05-02 13:28:51 -0600

    navigation section routes

--- apps/angular-app/src/app/app.routes.ts
+++ apps/angular-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { FormLoginComponent } from './views/form-login/form-login.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: 'categories', component: HomeComponent },
+    { path: 'login', component: FormLoginComponent },
+    { path: '',   redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', component: FormLoginComponent }
+];

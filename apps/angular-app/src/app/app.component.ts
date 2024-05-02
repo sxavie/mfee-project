5e1ef73045bea5cbe43f61560a11d1e028819a88
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-05-02 13:29:56 -0600

    app component updated

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { FormLoginComponent } from './views/form-login/form-login.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HomeComponent, CommonModule, HttpClientModule],
+  imports: [RouterModule, HomeComponent, CommonModule, HttpClientModule, MatButtonModule, MatMenuModule, FormLoginComponent ],
   selector: 'mfee-project-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'

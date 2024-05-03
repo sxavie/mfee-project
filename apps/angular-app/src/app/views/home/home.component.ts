1d8f1bb4b8f017770c29b49331e078cda05c312b
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-05-03 12:47:38 -0600

    categories list routes added && updated

--- apps/angular-app/src/app/views/home/home.component.ts
+++ apps/angular-app/src/app/views/home/home.component.ts
@@ -6,11 +6,12 @@ import { WorldCardComponent } from '../../components/world-card/world-card.compo
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { CategoriesComponent } from '../categories/categories.component';
 
 @Component({
   selector: 'mfee-project-home',
   standalone: true,
-  imports: [CommonModule, MainTitleComponent, WorldCardComponent, MatMenuModule, MatButtonModule],
+  imports: [CommonModule, MainTitleComponent, WorldCardComponent, MatMenuModule, MatButtonModule, CategoriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -28,15 +29,19 @@ export class HomeComponent {
     this.router.navigate(['/login']);
     switch (route) {
       case 'login':
-        console.log('to login')
+        console.log('to login');
         this.router.navigate(['/login']);
         break;
       case 'categories':
-        console.log('to categories')
+        console.log('to categories');
         this.router.navigate(['/categories']);
         break;
+      case 'categories-list':
+        console.log('to categories list');
+        this.router.navigate(['/categories-list']);
+        break;
       default:
-        this.router.navigate(['/login']); 
+        this.router.navigate(['/login']);
     }
   }
 }

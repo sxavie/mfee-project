fc4610695d0034d9d6d88168c3b5fcb127344c56
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-05-03 09:31:30 -0600

    signup route added

--- apps/angular-app/src/app/app.routes.ts
+++ apps/angular-app/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { FormLoginComponent } from './views/form-login/form-login.component';
+import { FormSignUpComponent } from './views/form-sign-up/form-sign-up.component';
 
 export const appRoutes: Route[] = [
     { path: 'categories', component: HomeComponent },
     { path: 'login', component: FormLoginComponent },
+    { path: 'sign-up', component: FormSignUpComponent },
     { path: '',   redirectTo: '/login', pathMatch: 'full' },
     { path: '**', component: FormLoginComponent }
 ];

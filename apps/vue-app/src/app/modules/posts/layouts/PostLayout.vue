bfc2ebc2d50032e819b21fa97b4041af2496c4dc
Alejandro Lagarde <c-alejandro.lagarde@charter.com>
2024-05-12 01:32:21 -0600

    Session 01

--- apps/vue-app/src/app/modules/posts/layouts/PostLayout.vue
+++ apps/vue-app/src/app/modules/posts/layouts/PostLayout.vue
@@ -1,4 +1,4 @@
-<!-- Activity 2: Create and render PostView.vue component -->
+<!-- Activity 2: Render PostView.vue component -->
 <template>
   <h1>TODO: Eliminar este h1 cuando se haga la actividad</h1>
 </template>

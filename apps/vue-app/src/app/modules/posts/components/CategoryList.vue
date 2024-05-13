b0861a618a1def70033bdb6508c16bbf98811d18
Alejandro Lagarde <c-alejandro.lagarde@charter.com>
2024-05-12 23:31:02 -0600

    branch session 02 ready

---
+++ apps/vue-app/src/app/modules/posts/components/CategoryList.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      /*   Activity 8: Add v-for directive: Use this array to iterate <CategoryItem> in the template */
+      categories: [
+        {
+          id: 2,
+          name: 'Travel'
+        },
+        {
+          id: 3,
+          name: 'Lifecycle'
+        },
+        {
+          id: 4,
+          name: 'Business'
+        },
+        {
+          id: 5,
+          name: 'Work'
+        }
+      ]
+    };
+  }
+  /*   Activity 5: Add created hook */
+};
+</script>
---
+++ apps/vue-app/src/app/modules/posts/views/PostDetailView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card bg-dark text-white">
+        <img src="https://cdn.pixabay.com/photo/2017/02/22/17/06/wave-2089959_960_720.jpg" class="card-img" />
+        <div class="card-img-overlay">
+          <div class="d-flex justify-content-start align-items-center ms-4">
+            <i class="fa-solid fa-chevron-left me-2"></i>
+            <span>View Posts</span>
+          </div>
+        </div>
+        <div class="card-img-overlay text-center title">
+          <div class="card-content">
+            <h1 class="display-2">
+              <strong>Post title</strong>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-12 bg-gray">
+      <div class="container m-5">
+        <p class="fs-5">Post Description</p>
+        <!-- Activity 7: Render components: Render CommentList from /components folder */ -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  /*   Activity 5: Add created hook */
+};
+</script>
+
+<style scoped>
+.card-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+.bg-gray {
+  background-color: #e7e7e7;
+}
+.title {
+  top: 40px;
+}
+</style>
